843c786d74a2489cbe76e12a7ad1b8f1e436356f
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2024-05-02 17:17:29 +0200

    Change dice to object and create its type interface

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,48 @@ import { Die } from './components/Die';
 import { DieBtn } from './components/DieBtn';
 import Close from "./assets/svg/close.svg";
 import RollDie from "./assets/svg/rollDie.svg";
+import { DieInt } from "./components/types";
 
 function App() {
   const dieSize = [2, 4, 6, 8, 10, 12, 20, 100];
-  const [dice, setDice] = useState<number[]>([]);
-  const [rolledNumbers, setRolledNumbers] = useState<number[]>([]);
-  const sum = rolledNumbers.reduce((prev, curr) => prev + curr, 0);
+  const [dice, setDice] = useState<DieInt[]>([]);
+  const sum = dice.reduce(sumRolls, 0);
 
   function roll() {
-    const randomNubers = dice.map((dieSize) => Math.floor(Math.random() * dieSize) + 1);
-    return setRolledNumbers([...randomNubers]);
+    const randomNubers = dice.map((die) => {
+      return { ...die, rolledNumber: Math.floor(Math.random() * die.size) + 1 }
+    });
+    return setDice(randomNubers);
+  }
+
+  function sumRolls(prev: number, curr: DieInt): number {
+    if (typeof curr.rolledNumber === "number") {
+      return prev + curr.rolledNumber;
+    }
+    return 0;
   }
 
   function addDice(size: number): void {
-    setDice([...dice, size]);
+    setDice([...dice, {
+      id: dice.length + 1,
+      size: size,
+      rolledNumber: null,
+    }]);
   }
 
   function reset() {
     setDice([]);
-    setRolledNumbers([]);
   }
 
   return (
     <main>
       <ul>
-        {dice.map((die, index) => <Die key={`die-${index}`} index={index} dieSize={die} rolledNumbers={rolledNumbers} />)}
+        {dice.map(die => <Die key={`die-${die.id}`} die={die} />)}
       </ul>
       <p className="sum">{sum}</p>
       <div className="buttons">
-        {dice.length > 0 && <button type="button" className="action" onClick={() => reset()}><Close /> Reset</button>}
-        {dice.length > 0 && <button type="button" className="action" onClick={() => roll()}><RollDie /> Roll</button>}
+        {dice.length > 0 && <button type="button" className="action" onClick={reset}><Close /> Reset</button>}
+        {dice.length > 0 && <button type="button" className="action" onClick={roll}><RollDie /> Roll</button>}
       </div>
       <div className="buttons dice">
         {dieSize.map((die) => <DieBtn addDice={addDice} dieSize={die} key={`d${die}`} />)}
---
+++ src/components/types.ts
@@ -0,0 +1,5 @@
+export interface DieInt {
+    id: number,
+    size: number,
+    rolledNumber: null | number,
+}
